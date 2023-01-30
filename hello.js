3f9e892f4f7b8c899b00951e22de044b5e9421d5
aavik743 <aavikhore@gmail.com>
2023-01-30 14:44:03 +0530

    added package.json file

--- hello.js
+++ hello.js
@@ -1,6 +1,22 @@
 var http = require('http');
+var fs = require('fs');
+var url = require('url');
 
 http.createServer(function(req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('<h1> Hello World </h1>');
-}).listen(8888)
+    var q = url.parse(req.url, true); // req.url = /
+    var filename = "." + q.pathname + ".html"; // q.pathname is params or path
+
+    if (filename == './.html') { filename = './index.html'}
+
+    fs.readFile(filename, function(err, data){
+        if (err) {
+            res.writeHead(404, {'Content-Type': 'text/html'});
+            return res.end("404 Not Found");
+        }
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        return res.end();
+    });
+}).listen(8080)
+
+console.log("...yoooooo!!, server run'in...")
